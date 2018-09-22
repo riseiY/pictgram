a28fbe36dde2eccc36b0bf8f0bb2e9e27343267a
Risei <tenisusumasyu3@icloud.com>
2018-09-22 17:20:33 +0900

    forth

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -5,6 +5,9 @@ class Topic < ApplicationRecord
   validates :image, presence:true
 
   belongs_to :user
+  has_many :favorites
 
   mount_uploader :image, ImageUploader
+
+  has_many :favorite_users, through: :favorites, source: 'user'
 end
